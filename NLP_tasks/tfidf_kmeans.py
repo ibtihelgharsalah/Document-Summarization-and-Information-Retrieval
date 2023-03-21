fadd52161a3f1051360e1a8d296f03477e035100
Ibtihel Gharsalah <gharsalahibtihel732@gmail.com>
2023-03-21 11:53:59 +0100

    displays keywords along with each topic

--- NLP_tasks/tfidf_kmeans.py
+++ NLP_tasks/tfidf_kmeans.py
@@ -1,5 +1,7 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.cluster import KMeans
+import numpy
+from numpy import argsort
 from NLP_preprocessing.data_cleaning import eng_clean, fr_clean
 from nltk.corpus import stopwords
 
@@ -20,6 +22,20 @@ def group_by_topic_english(eng_files):
     eng_kmeans = KMeans(n_clusters=2, random_state=0).fit(eng_tfidf_matrix)
     # Extract the labels from the K-means models
     eng_labels = eng_kmeans.labels_
+    # Extract the centroids of the two clusters
+    eng_centroids = eng_kmeans.cluster_centers_
+    
+    # Use the centroids to find the top keywords for each cluster
+    n_top_keywords = 10
+    eng_feature_names = eng_vectorizer.get_feature_names_out()
+    eng_topics_keywords = {}
+    for i in range(len(eng_centroids)):
+        centroid = eng_centroids[i]
+        # Sort the indices of the features based on their importance
+        sorted_indices = centroid.argsort()[::-1][:n_top_keywords]
+        # Get the keywords for the sorted indices
+        top_keywords = [eng_feature_names[index] for index in sorted_indices]
+        eng_topics_keywords[f'Topic {i+1}'] = top_keywords
     
     # Create empty lists for the English files for each topic
     eng_topic1, eng_topic2 = [], []
@@ -32,7 +48,10 @@ def group_by_topic_english(eng_files):
             eng_topic2.append(file_name)
     
     # Create a dictionary of topics
-    eng_topics = {'Topic 1': eng_topic1, 'Topic 2': eng_topic2}
+    eng_topics = {}
+    eng_topics[f"Topic 1: ({', '.join(eng_topics_keywords['Topic 1'])})"] = eng_topic1
+    eng_topics[f"Topic 2: ({', '.join(eng_topics_keywords['Topic 2'])})"] = eng_topic2
+        
     return eng_topics
 
 
@@ -56,8 +75,22 @@ def group_by_topic_french(fr_files):
     fr_kmeans = KMeans(n_clusters=2, random_state=0).fit(fr_tfidf_matrix)
     # Extract the labels from the K-means models
     fr_labels = fr_kmeans.labels_
+    # Extract the centroids of the two clusters
+    fr_centroids = fr_kmeans.cluster_centers_
     
-    # Create empty lists for the French files for each topic
+    # Use the centroids to find the top keywords for each cluster
+    n_top_keywords = 10
+    fr_feature_names = fr_vectorizer.get_feature_names_out()
+    fr_topics_keywords = {}
+    for i in range(len(fr_centroids)):
+        centroid = fr_centroids[i]
+        # Sort the indices of the features based on their importance
+        sorted_indices = centroid.argsort()[::-1][:n_top_keywords]
+        # Get the keywords for the sorted indices
+        top_keywords = [fr_feature_names[index] for index in sorted_indices]
+        fr_topics_keywords[f'Topic {i+1}'] = top_keywords
+    
+    # Create empty lists for the Frencg files for each topic
     fr_topic1, fr_topic2 = [], []
     # Loop through each file and classify it into the appropriate topic
     for file_name, text in fr_files.items():
@@ -68,5 +101,8 @@ def group_by_topic_french(fr_files):
             fr_topic2.append(file_name)
     
     # Create a dictionary of topics
-    fr_topics = {'Topic 1': fr_topic1, 'Topic 2': fr_topic2}
+    fr_topics = {}
+    fr_topics[f"Topic 1: ({', '.join(fr_topics_keywords['Topic 1'])})"] = fr_topic1
+    fr_topics[f"Topic 2: ({', '.join(fr_topics_keywords['Topic 2'])})"] = fr_topic2
+    
     return fr_topics
